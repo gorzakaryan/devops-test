5fcba5adb5c9cc99c4b77cd1e90a50ec494608ba
Gor Zakaryan <gormzakaryan@gmail.com>
2026-05-16 15:53:10 +0400

    Changed deploy.yml file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,6 +21,7 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: Number(import.meta.env.VITE_REVERB_PORT),
+    wssPort: Number(import.meta.env.VITE_REVERB_PORT),
     forceTLS: false,
-    enabledTransports: ['ws'],
+    enabledTransports: ['ws', 'wss'],
 });
